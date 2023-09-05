c3ba5cf6fb3a5f8f8a7e55e94a9aa46b95be6a5c
Carlos Marques <kadupmarques@gmail.com>
2023-09-05 09:27:01 -0300

    config: Jest Configuration

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -2,7 +2,7 @@ import * as Create from './Create'
 import * as GetAll from './GetAll'
 import * as GetById from './GetByID'
 import * as UpdateById from './UpdateByID'
-import * as DeleteById from './DeleteById'
+import * as DeleteById from './DeleteByID'
 
 export const CidadesController = {
     ...Create,
