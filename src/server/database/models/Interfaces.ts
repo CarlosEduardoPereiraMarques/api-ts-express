26dba91d3dc0bb5c99b58bdc19e540b860ed0313
Carlos Marques <kadupmarques@gmail.com>
2023-09-14 16:52:11 -0300

    create: Creating Pessoas and Usuario interfaces

--- src/server/database/models/Interfaces.ts
+++ src/server/database/models/Interfaces.ts
@@ -1,14 +1,19 @@
 
-
 export interface ICidade{
     id: number;
     nome: string;
 }
 
 export interface IPessoa {
-
+    id: number
+    nomeCompleto: string
+    email: string
+    cidadeId: number
 }
 
 export interface IUsuario {
-    
+    id: number
+    nome: string
+    email: string
+    senha: string
 }
